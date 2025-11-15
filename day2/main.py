2f2bc1ac8f6c9405f8817c59d5c9b0b672f82baa
Ved Badadhe <vbbadadhe@gmail.com>
2025-11-15 17:30:16 +0530

    Day2 Done

---
+++ day2/main.py
@@ -0,0 +1,24 @@
+import json
+from queue import Queue
+
+q = Queue()
+with open("day2/bucket.json" , "r") as f:
+    data = json.load(f)
+
+
+for bucket in data["buckets"]:
+    # print("Name is" , bucket["name"])
+    # for key,value in bucket["tags"].items():
+    #     print(f"  {key}: {value}")
+    
+    # for policy in bucket["policies"]:
+    #     print("Type is " , policy["type"])
+
+    if bucket["sizeGB"] > 50:
+        q.put(bucket)
+
+
+print("Buckets to be deleted are the")
+while not q.empty():        
+    item = q.get()        
+    print(item["name"])
